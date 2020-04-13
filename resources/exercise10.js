d6ac52ab0a8119a4ed83013b4a149fca75a2fde9
Lim Leong Kui <leongkui1976@gmail.com>
2020-04-13 09:57:07 +0800

    fix an error in execise10

--- resources/exercise10.js
+++ resources/exercise10.js
@@ -1,4 +1,4 @@
-db.nodes.insert({ '_id': 'CSS', 'name': 'CEO', 'minions': ['LLK', 'CL'] });
+db.nodes.insert({ '_id': 'CEO', 'name': 'CEO', 'minions': ['LLK', 'CL'] });
 db.nodes.insert({ '_id': 'LLK', 'name': 'LLK', 'minions': ['LST'] });
 db.nodes.insert({ '_id': 'LST', 'name': 'LST', 'minions': [] });
 db.nodes.insert({ '_id': 'CL', 'name': 'CL', 'minions': ['CCY','IB','L'] });
@@ -6,7 +6,7 @@ db.nodes.insert({ '_id': 'CCY', 'name': 'CCY', 'minions': ['AQ','CCK'] });
 db.nodes.insert({ '_id': 'CCK', 'name': 'CCK', 'minions': [] });
 db.nodes.insert({ '_id': 'AQ', 'name': 'AQ', 'minions': [] });
 db.nodes.insert({ '_id': 'IB', 'name': 'I', 'minions': [] });
-db.nodes.insert({ '_id': 'N', 'name': 'N from Yishun', 'minions': [] });
+db.nodes.insert({ '_id': 'N', 'name': 'N', 'minions': [] });
 db.nodes.insert({ '_id': 'S', 'name': 'S', 'minions': [] });
 db.nodes.insert({ '_id': 'L', 'name': 'L', 'minions': [] });
 
@@ -28,7 +28,7 @@ db.nodes.aggregate([
   }
 ]).pretty();
 
-db.nodes.update({'_id':'CSS'},{'$addToSet':{'minions':'CL'}};
+db.nodes.update({'_id':'CEO'},{'$addToSet':{'minions':'CL'}};
 
 db.nodes.aggregate([
   {
